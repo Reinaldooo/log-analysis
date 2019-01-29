73a80853f0c215aab4d50a68ffc0a6b954259a28
Reinaldo Trindade <rewifetri@gmail.com>
2019-01-29 21:54:29 -0200

    Logic for most failed requests complete

--- newsdb.py
+++ newsdb.py
@@ -7,11 +7,48 @@ def top_three_articles():
   db = psycopg2.connect(database=DBNAME)
   c = db.cursor()
   c.execute("""
-  select title, hits from articles, pathHits
-  where articles.slug = replace(pathHits.path, '/article/', '') limit 3;
+  SELECT title, hits FROM articles, path_hits
+  WHERE articles.slug = replace(path_hits.path, '/article/', '') LIMIT 3;
   """)
   result = c.fetchall()
-  print("TOP 3 most viewed articles of all time: \n")
+  list_item = 1
+  print("Top 3 most viewed articles of all time: \n")
   for item in result:
-    print("Title: %s - Views: %s" %(item[0],item[1],))
+    print("%s - Title: %s" %(list_item, item[0],))
+    print("Views: %s" %(item[1],))
+    list_item += 1
+  db.close()
+
+def top_authors():
+  """Displays top authors based on the sum of views in their articles"""
+  db = psycopg2.connect(database=DBNAME)
+  c = db.cursor()
+  c.execute("""
+  SELECT authors_table.name, sum(path_hits.hits) AS total
+  FROM authors_table, path_hits
+  WHERE authors_table.slug = replace(path_hits.path, '/article/', '')
+  GROUP BY authors_table.name
+  ORDER BY total DESC;
+  """)
+  result = c.fetchall()
+  list_item = 1
+  print("Top authors based on overall views: \n")
+  for item in result:
+    print("%s - Name: %s - Views: %s" %(list_item, item[0],item[1],))
+    list_item += 1
+  db.close()
+
+def top_failed_requests():
+  """Displays dates where more than 1% of requests failed"""
+  db = psycopg2.connect(database=DBNAME)
+  c = db.cursor()
+  c.execute("""
+  SELECT date, percentage from date_reqs where percentage > 1;
+  """)
+  result = c.fetchall()
+  list_item = 1
+  print("Dates where more than 1% of requests failed: \n")
+  for item in result:
+    print("%s - Date: %s - Failed Percentage: %s" %(list_item, item[0],item[1],))
+    list_item += 1
   db.close()
