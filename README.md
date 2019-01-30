5b81b94181dc0d06ebe4191cd936cfa36d290d8d
Reinaldo Trindade <rewifetri@gmail.com>
2019-01-29 22:08:54 -0200

    README inclusion

---
+++ README.md
@@ -0,0 +1,15 @@
+## Backend Developer Nanodegree Program
+### Log Analysis
+
+This tool will use Python to analise the PostgresSQL database of an newspaper site and answer 3 big questions:
+
+![Example PNG](/Example.png)
+
+<dl>
+  <dd>Top 3 most viewed articles</dd>
+  <dd>Most Popular Authors</dd>
+  <dd>Days where site requests failed more than 1% of the time</dd>
+</dl>
+
+
+###### Reinaldo Trindade - Full Stack Developer
--- views.sql
+++ views.sql
@@ -1,30 +1,30 @@
--- CREATE VIEW path_hits AS
---     SELECT path, count(*) AS hits FROM log
---     WHERE log.status = '200 OK' AND
---     path LIKE '%article%'
---     GROUP BY path
---     ORDER BY hits DESC;
+CREATE VIEW path_hits AS
+    SELECT path, count(*) AS hits FROM log
+    WHERE log.status = '200 OK' AND
+    path LIKE '%article%'
+    GROUP BY path
+    ORDER BY hits DESC;
 
--- CREATE VIEW authors_table AS
---     SELECT articles.slug, authors.name FROM articles, authors 
---     WHERE authors.id = articles.author;
+CREATE VIEW authors_table AS
+    SELECT articles.slug, authors.name FROM articles, authors 
+    WHERE authors.id = articles.author;
 
--- CREATE VIEW req_errors AS
---   SELECT time::date AS date, count(*) AS total FROM log
---     WHERE status != '200 OK'
---     GROUP BY log.time::date
---     ORDER BY total Desc;
+CREATE VIEW req_errors AS
+  SELECT time::date AS date, count(*) AS total FROM log
+    WHERE status != '200 OK'
+    GROUP BY log.time::date
+    ORDER BY total Desc;
 
--- CREATE VIEW req_total AS
---   SELECT time::date AS date, count(*) AS total FROM log
---     GROUP BY log.time::date
---     ORDER BY total Desc;
+CREATE VIEW req_total AS
+  SELECT time::date AS date, count(*) AS total FROM log
+  GROUP BY log.time::date
+  ORDER BY total Desc;
 
--- CREATE VIEW date_reqs AS
--- SELECT req_total.date,
--- req_total.total AS req_ok,
--- req_errors.total AS req_err,
--- round(( req_errors.total::decimal / req_total.total ) * 100) AS percentage
--- FROM req_total, req_errors
--- WHERE req_total.date = req_errors.date
--- ORDER BY req_ok DESC;
+CREATE VIEW date_reqs AS
+  SELECT req_total.date,
+  req_total.total AS req_ok,
+  req_errors.total AS req_err,
+  round(( req_errors.total::decimal / req_total.total ) * 100) AS percentage
+  FROM req_total, req_errors
+  WHERE req_total.date = req_errors.date
+  ORDER BY req_ok DESC;
